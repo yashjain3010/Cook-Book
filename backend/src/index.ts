8d0f8baa52eaa73ca2416e8d338697ccf69da268
Yash Jain <yashjain30102000@gmail.com>
2024-08-02 12:04:15 +0530

    Backend Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
-import express, { Application } from 'express';
+import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
 
-const app: Application = express();
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.use(helmet());
 
-const PORT = 3000;
+app.get("/api/recipes/search",async (req,res) => {
+    res.json({
+        message : 'Success'
+    })
+})
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.listen(3000, () => {
+    console.log("Server Running on localhost:3000")
 });
